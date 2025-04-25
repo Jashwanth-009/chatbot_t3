24bb3356b6e5bb69439e6a67fa805a6f28f66ab8
Jashwanth-009 <jashwanthbommakanti6@gmail.com>
2025-04-25 18:06:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -271,3 +271,5 @@ if __name__ == '__main__':
         ask_from_cli()
     else:
         app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port)
